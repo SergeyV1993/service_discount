301c5e05f8d610e884067ab1a049caf83bc3fc73
SergeyV1993 <sergey.vlasov3333@gmail.com>
2019-05-23 20:19:28 +0300

    edit admin fields

--- discount_cart/admin.py
+++ discount_cart/admin.py
@@ -5,6 +5,7 @@ from .models import *
 @admin.register(DiscountCart)
 class DiscountCartAdmin(admin.ModelAdmin):
     list_display = [field.name for field in DiscountCart._meta.fields]
+    search_fields = ['id', 'cart', 'code']
 
     class Meta:
         model = DiscountCart
--- discount_cart/models.py
+++ discount_cart/models.py
@@ -3,12 +3,16 @@ from datetime import *
 
 
 class DiscountCart(models.Model):
-    cart = models.IntegerField(null=True, blank=True, db_index=True)
-    code = models.CharField(max_length=20, blank=True, null=True, default=None, db_index=True)
-    valid_date_start = models.DateTimeField(auto_now_add=True, auto_now=False)
-    valid_date_end = models.DateTimeField(default=datetime.now(timezone.utc) + timedelta(days=90))
-    nominal = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    status = models.BooleanField(default=False)
+    cart = models.IntegerField(null=True, blank=True, db_index=True, verbose_name='Корзина')
+    code = models.CharField(max_length=20, blank=True, null=True, default=None, db_index=True, verbose_name='Промо код')
+    valid_date_start = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name='Начало действия')
+    valid_date_end = models.DateTimeField(default=datetime.now(timezone.utc) + timedelta(days=90), verbose_name='Конец действия')
+    nominal = models.DecimalField(max_digits=10, decimal_places=2, default=0, verbose_name='Номинал скидки')
+    status = models.BooleanField(default=False, verbose_name='Статус')
+
+    class Meta:
+        verbose_name_plural = 'Скидки'
+        verbose_name = 'Скидка'
 
     def __str__(self):
         try:
