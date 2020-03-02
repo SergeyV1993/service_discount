79865957ea40f8b934c1f0f2a8b667b3ee5e2805
Vlasov Sergei <vlasov@vlasov-macbookair-2.local>
2020-03-02 20:25:02 +0300

    rename attr

--- discount_cart/views.py
+++ discount_cart/views.py
@@ -7,23 +7,23 @@ from .serializers import *
 
 @api_view(['POST'])
 def post(request):
-    code = DiscountPostSerializer(data=request.data)
+    discount_data = DiscountPostSerializer(data=request.data)
 
-    if code.is_valid():
-        if DiscountCart.objects.filter(code=code.validated_data['code']).exists():
-            discount = DiscountCart.objects.get(code=code.validated_data['code'])
+    if discount_data.is_valid():
+        if DiscountCart.objects.filter(code=discount_data.validated_data['code']).exists():
+            discount = DiscountCart.objects.get(code=discount_data.validated_data['code'])
         else:
             return Response({"message": "Code does not exist"}, status=status.HTTP_404_NOT_FOUND)
 
         if discount.status and discount.valid_date_end > datetime.now(timezone.utc):
-            discount.cart, discount.status = code.validated_data['cart'], False
+            discount.cart, discount.status = discount_data.validated_data['cart'], False
             discount.save(update_fields=["cart", "status"])
 
-            cart_sum = float(code.validated_data['amount_cart'])
+            cart_sum = float(discount_data.validated_data['amount_cart'])
             new_sum_cart = cart_sum - float(discount.nominal)
             if new_sum_cart < 0:
                 new_sum_cart = 0
-            return Response({"amount_cart": new_sum_cart, "cart": code.validated_data['cart']},)
+            return Response({"amount_cart": new_sum_cart, "cart": discount_data.validated_data['cart']},)
         else:
             return Response({"message": "Expired"}, status=status.HTTP_403_FORBIDDEN)
     else:
