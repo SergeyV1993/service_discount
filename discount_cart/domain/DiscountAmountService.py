5dd0d176f4a66f0cd255152f0a0839f113c146ad
Vlasov Sergei <vlasov@vlasov-macbookair-2.local>
2020-08-06 12:22:09 +0300

    add tests + fix calculate amount for cart + fix requirements.txt

--- discount_cart/domain/DiscountAmountService.py
+++ discount_cart/domain/DiscountAmountService.py
@@ -4,4 +4,9 @@ from discount_cart.domain.DiscountAmountServiceInterface import DiscountAmountIn
 class DiscountAmount(DiscountAmountInterface):
 
     def calculate_discount_amount_cart(self, cart_amount: float, discount_amount: float) -> float:
-        return round(cart_amount - discount_amount, 2)
+        total_discount_amount = round(float(cart_amount) - float(discount_amount), 2)
+
+        if total_discount_amount < 0:
+            return 0.0
+
+        return total_discount_amount
